6d92fd3cb2caf1a857cdacc534cca99c79167d92
Thang Nguyen <nguyendinhthanght@gmail.com>
2023-11-17 17:35:16 -0600

    change car detail => add cart feature

--- src/controllers/car-detail/index.js
+++ src/controllers/car-detail/index.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import useAPI from "../../api";
 import { useNavigate, useParams } from "react-router-dom";
-import { Col, Container, Row, Button } from "react-bootstrap";
+import { Col, Container, Row, Button, Form } from "react-bootstrap";
 import { ImageGallery, OptionType } from "./components";
 import { useDispatch } from "react-redux";
 import "./index.css";
@@ -14,6 +14,7 @@ export const CarDetail = () => {
     const [totalPrice, setTotalPrice] = useState(1);
     const dispatch = useDispatch();
     const navigate = useNavigate();
+    const [select, setSelect] = useState(false);
 
     useEffect(() => {
         async function fetching() {
@@ -44,25 +45,36 @@ export const CarDetail = () => {
         typeItem.items[itemIndex] = attribute;
         temp.attributeTypes[index] = typeItem;
         setCar(temp);
-        calculateTotalPrice(temp);
+        calculateTotalPrice(temp, number);
     }
 
-    const calculateTotalPrice = (car) => {
+    const calculateTotalPrice = (car, number) => {
         let price = car.basePrice;
         car.attributeTypes.forEach(type => {
-            type.items.filter(e=> e.selected === true).forEach(item => {
+            type.items.filter(e => e.selected === true).forEach(item => {
                 price += item.additionalPrice;
             })
         });
-        setTotalPrice(price);
+        setTotalPrice(price * number);
     }
 
-    const open = (e) => {
+    const addToCart = (e) => {
         e.preventDefault();
         dispatch({ type: "addcart", item: car, number: number })
         navigate("/carts");
     }
 
+    const numberChange = (e) => {
+        e.preventDefault();
+        const value = parseInt(e.target.value);
+        if (value) {
+            setSelect(true);
+            setNumber(value);
+            calculateTotalPrice(car,value);
+        } else {
+            setSelect(false);
+        }
+    }
     return (
         <div className="car">
             {car && (
@@ -93,25 +105,38 @@ export const CarDetail = () => {
                         ))}
                     </Row>
 
+                    <Row className="text-center">
+                        <h5 id="title" className="card-title">HOW MANY DO YOU WANT TO BUY?</h5>
+
+                        <Col lg={5} />
+                        <Col lg={2} className="mb-6 mb-lg-0 text-center select"  >
+                            <Form.Select onChange={numberChange} aria-label="Default select example">
+                                <option>Select number</option>
+                                <option value="1">1</option>
+                                <option value="2">2</option>
+                                <option value="3">3</option>
+                                <option value="4">4</option>
+                            </Form.Select>
+                        </Col>
+                        <Col lg={5} />
+                    </Row>
+
                     <Row>
-                        <Col xs={12} className="mb-6 mb-lg-0 text-center"  >
-                            <h4 id="total" className="card-title">Total Price: <span className="text-success">${totalPrice}</span> </h4> 
-                   
-
+                        <Col xs={12} className="mb-6 mb-lg-0 text-center total" >
+                            <h4 id="total" className="card-title">Total Price: <span className="text-success">${totalPrice}</span> </h4>
                         </Col>
 
-                        <Col lg={3}/>
+                        <Col lg={3} />
                         <Col lg={6} className=" gap-2 text-center addcart">
-                         <Button onClick={open} size="lg" variant="dark">Add To Cart</Button>
+                            <Button onClick={addToCart} disabled={!select} size="lg" variant="dark">Add To Cart</Button>
                         </Col>
-                        <Col lg={3}/>
+                        <Col lg={3} />
 
                     </Row>
 
                 </Container>
             )}
         </div>
-
     )
 
 }
