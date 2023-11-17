ff6f78457b0c0b83591c7478f93c1fc964589c9a
Thang Nguyen <nguyendinhthanght@gmail.com>
2023-11-17 11:44:44 -0600

    add car detail info

--- src/controllers/car-detail/index.js
+++ src/controllers/car-detail/index.js
@@ -1,8 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import useAPI from "../../api";
+import { useParams } from "react-router-dom";
 
 
 export const CarDetail = () => {
+    const params = useParams();
+    const {GetClient} = useAPI();
+    const [car, setCar] = useState({});
 
+    useEffect(()=>{
+        async function fetching () {
+            await loadCar(params.sku);
+        }
+        fetching();
+    },[])
+
+    const loadCar = async (sku) => {
+        const response = await GetClient("/api/cars/"+ sku);
+        if (response.status === 200) {
+            setCar(response.data);
+            console.log(response.data);
+        }
+    }
     return (<>
         <h2>Car Detail</h2>
     </>)
