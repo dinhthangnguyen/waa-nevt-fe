6c4de61be1d8c6b1a0995b75ffc803271132c291
Thang Nguyen <nguyendinhthanght@gmail.com>
2023-11-16 21:21:12 -0600

    fix local storage

--- src/navbar/index.js
+++ src/navbar/index.js
@@ -5,30 +5,30 @@ import NavDropdown from 'react-bootstrap/NavDropdown';
 import { useSelector } from 'react-redux';
 
 function CustomNavBar() {
-  const user = useSelector(state=>state.user);
+  const user = useSelector(state => state.user);
 
   return (
     <Navbar bg="dark" data-bs-theme="dark">
-    <Container>
+      <Container>
         <Navbar.Brand href="#home">NEVT</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link href="/login">TOP SELLING</Nav.Link>
             <Nav.Link href="/cars">BUY A CAR</Nav.Link>
-            <NavDropdown title="LOGIN" id="basic-nav-dropdown">
+            {!user && <NavDropdown title="LOGIN" id="basic-nav-dropdown">
               <NavDropdown.Item href="/login">Log In</NavDropdown.Item>
               <NavDropdown.Item href="/signup">Sign Up</NavDropdown.Item>
               <NavDropdown.Divider />
               <NavDropdown.Item href="/logout">Log out</NavDropdown.Item>
 
-            </NavDropdown>
+            </NavDropdown>}
 
-            <NavDropdown title={user.firstName + " " + user.lastName} id="basic-nav-dropdown">
+            {user && <NavDropdown title={user.firstName + " " + user.lastName} id="basic-nav-dropdown">
               <NavDropdown.Divider />
               <NavDropdown.Item href="/logout">Log out</NavDropdown.Item>
 
-            </NavDropdown>
+            </NavDropdown>}
           </Nav>
         </Navbar.Collapse>
       </Container>
--- src/store/store.js
+++ src/store/store.js
@@ -1,15 +1,19 @@
 import { configureStore } from "@reduxjs/toolkit"
 
-const initialData = {token: "", user: {token: "", email: "", firstName: "", lastName: ""}, shoppingCart: []};
+const user = localStorage.getItem("myuser") === null ? null : JSON.parse(localStorage.getItem("myuser"));
+const token = localStorage.getItem("token") === null ? "" : localStorage.getItem("token");
+const initialData = { token: token, user: user, shoppingCart: [] };
 
 const reducer = (state = initialData, action) => {
     if (action.type === "login") {
-        return {...state, user: action.user, token: action.user.token};
+        localStorage.setItem("token", action.user.token);
+        localStorage.setItem("myuser", JSON.stringify(action.user));
+        return { ...state, user: action.user, token: action.user.token };
     }
 
     return state;
 }
 
-const store = configureStore({reducer: reducer});
+const store = configureStore({ reducer: reducer });
 
 export default store;
