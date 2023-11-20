e749cbb62b8d9cd129fdfeb551f4ead5d5bf7248
Thang Nguyen <nguyendinhthanght@gmail.com>
2023-11-20 14:15:41 -0600

    refactor order page, add address

--- src/controllers/orders/index.js
+++ src/controllers/orders/index.js
@@ -1,32 +1,12 @@
-import { Button, Card, Col, Container, Row } from "react-bootstrap"
+import { Col, Container, Row } from "react-bootstrap"
 import { useSelector } from "react-redux"
 import { useEffect, useState } from "react";
 import useAPI from "../../api";
 import { useNavigate } from "react-router-dom";
-import { CartCell } from "../../components";
+import { OrderCell } from "../../components";
 import "./index.css";
 
-const OrderItem = ({ order }) => {
-    return (
-        <Container className="cartitem">
-            <Row >
-                <Col lg={12}>
-                    <Card.Title>ORDER STATUS: {order.orderStatus}</Card.Title>
-                    <Card.Text>
-                        Total: <span className="text-success">${order.total}</span>
-                    </Card.Text>
-                </Col>
-                {order.items.map(e => (
-                    <Col lg={12} key={e.car.productNumber}>
-                        <CartCell item={e} deleteF={null} onNumberChange={null} onReview={true} />
-                    </Col>
-                ))}
-            </Row>
 
-        </Container>
-
-    )
-}
 export const OrderPage = () => {
     const user = useSelector(state => state.user);
     const [orders, setOrders] = useState([]);
@@ -62,27 +42,11 @@ export const OrderPage = () => {
             <Row>
                 {orders.map(e => (
                     <Col lg={12} key={e.order}>
-                        <OrderItem order={e} />
+                        <OrderCell order={e} />
                     </Col>
                 ))}
             </Row>
-
-            {/* <Row>
-                <Col xs={12} className="mb-6 mb-lg-0 text-center total" >
-                    <h4 id="total" className="card-title">Total Price: <span className="text-success">${total}</span> </h4>
-                </Col>
-
-                <Col lg={3} />
-                <Col lg={6} className=" gap-2 text-center addcart">
-                    <Button className="btn btn-dark" onClick={checkout} size="lg" variant="dark">Checkout</Button>
-                </Col>
-                <Col lg={3} />
-
-            </Row> */}
-
-
         </Container>
-
     )
 }
 
