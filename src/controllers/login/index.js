2f767c9cf7ca69db0a4ece2b891ac5a4fdb357b4
Thang Nguyen <nguyendinhthanght@gmail.com>
2023-11-20 18:07:44 -0600

    add login and signup validation

--- src/controllers/login/index.js
+++ src/controllers/login/index.js
@@ -72,7 +72,7 @@ export const Login = () => {
                         <Form.Group className="mb-3" controlId="formBasicEmail">
                             <Form.Label>Email address</Form.Label>
                             <Form.Control type="email" placeholder="Enter email" name="email" value={user.email} onChange={handleFieldChange} />
-                            {Object.keys(passError).length === 0 &&
+                            {Object.keys(emailError).length === 0 &&
                                 <Form.Text className="text-muted">
                                     We'll never share your email with anyone else.
                                 </Form.Text>}
